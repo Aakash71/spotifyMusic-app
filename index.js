68e1a636ad5f0bd7b36d550880d4e2c003314681
Kondapalli <sri.a.kondapalli@accenture.com>
2024-06-20 12:11:20 +0530

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 /**
  * @format
  */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import TrackPlayer from 'react-native-track-player';
+import { name as appName } from './app.json';
+import { playbackService } from './musicPlayerServices';
+import App from './src/App';
 
 AppRegistry.registerComponent(appName, () => App);
+TrackPlayer.registerPlaybackService(() => playbackService);
